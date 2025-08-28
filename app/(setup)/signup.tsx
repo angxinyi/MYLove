bc11a4d4556ea8428261d0f941c33224af0a0861
angxinyi <angxinyi.school@gmail.com>
2025-08-28 06:05:07 +0100

    UI for signup page

--- app/(setup)/signup.tsx
+++ app/(setup)/signup.tsx
@@ -1,3 +1,4 @@
+import { Picker } from "@react-native-picker/picker";
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
 import {
@@ -26,19 +27,22 @@ export default function SignupPage() {
       <Image
         source={require("@/assets/images/logo-name.png")}
         style={styles.logo}
-        resizeMode="covered"
+        resizeMode="cover"
       />
 
       {/* Title */}
       <Text style={styles.title}>Let’s Get Started</Text>
 
       {/* Input Fields */}
+      <Text style={styles.label}>Name</Text>
       <TextInput
         placeholder="Name"
         value={name}
         onChangeText={setName}
         style={styles.input}
       />
+
+      <Text style={styles.label}>Email</Text>
       <TextInput
         placeholder="Email"
         value={email}
@@ -46,6 +50,8 @@ export default function SignupPage() {
         keyboardType="email-address"
         style={styles.input}
       />
+
+      <Text style={styles.label}>Password</Text>
       <TextInput
         placeholder="Password"
         value={password}
@@ -55,55 +61,59 @@ export default function SignupPage() {
       />
 
       {/* Date Picker */}
-      <Text style={styles.dateLabel}>When did your love story begin?</Text>
-      {/* <View style={styles.dateContainer}>
-        <Picker
-          selectedValue={day}
-          style={styles.picker}
-          onValueChange={(itemValue) => setDay(itemValue)}
-        >
-          <Picker.Item label="Day" value="Day" />
-          {[...Array(31)].map((_, i) => (
-            <Picker.Item key={i} label={`${i + 1}`} value={`${i + 1}`} />
-          ))}
-        </Picker>
-
-        <Picker
-          selectedValue={month}
-          style={styles.picker}
-          onValueChange={(itemValue) => setMonth(itemValue)}
-        >
-          <Picker.Item label="Month" value="Month" />
-          {[
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec",
-          ].map((m, i) => (
-            <Picker.Item key={i} label={m} value={m} />
-          ))}
-        </Picker>
-
-        <Picker
-          selectedValue={year}
-          style={styles.picker}
-          onValueChange={(itemValue) => setYear(itemValue)}
-        >
-          <Picker.Item label="Year" value="Year" />
-          {[...Array(50)].map((_, i) => {
-            const y = new Date().getFullYear() - i;
-            return <Picker.Item key={i} label={`${y}`} value={`${y}`} />;
-          })}
-        </Picker>
-      </View> */}
+      <Text style={styles.label}>When did your love story begin?</Text>
+      <View style={styles.dateContainer}>
+        <View style={styles.pickerDay}>
+          <Picker
+            selectedValue={day}
+            style={styles.pickerInner}
+            onValueChange={(itemValue) => setDay(itemValue)}
+          >
+            <Picker.Item label="Day" value="Day" />
+            {[...Array(31)].map((_, i) => (
+              <Picker.Item key={i} label={`${i + 1}`} value={`${i + 1}`} />
+            ))}
+          </Picker>
+        </View>
+        <View style={styles.pickerMonth}>
+          <Picker
+            selectedValue={month}
+            style={styles.pickerInner}
+            onValueChange={(itemValue) => setMonth(itemValue)}
+          >
+            <Picker.Item label="Month" value="Month" />
+            {[
+              "Jan",
+              "Feb",
+              "Mar",
+              "Apr",
+              "May",
+              "Jun",
+              "Jul",
+              "Aug",
+              "Sep",
+              "Oct",
+              "Nov",
+              "Dec",
+            ].map((m, i) => (
+              <Picker.Item key={i} label={m} value={m} />
+            ))}
+          </Picker>
+        </View>
+        <View style={styles.pickerYear}>
+          <Picker
+            selectedValue={year}
+            style={styles.pickerInner}
+            onValueChange={(itemValue) => setYear(itemValue)}
+          >
+            <Picker.Item label="Year" value="Year" />
+            {[...Array(50)].map((_, i) => {
+              const y = new Date().getFullYear() - i;
+              return <Picker.Item key={i} label={`${y}`} value={`${y}`} />;
+            })}
+          </Picker>
+        </View>
+      </View>
 
       {/* Sign Up Button */}
       <TouchableOpacity style={styles.signUpButton}>
@@ -130,7 +140,7 @@ export default function SignupPage() {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
+    backgroundColor: "#fef9f2",
     padding: 20,
     alignItems: "center",
     justifyContent: "center",
@@ -139,65 +149,84 @@ const styles = StyleSheet.create({
   },
   logo: {
     width: 140,
-    height: 40,
-    marginBottom: 30,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 4,
-    elevation: 5,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 10,
+    height: 50,
+    marginBottom: 20,
   },
   title: {
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: "bold",
+    color: "#2E2E2E",
     marginBottom: 30,
   },
   input: {
     width: "100%",
     borderWidth: 1,
-    borderColor: "#ccc",
     borderRadius: 8,
     padding: 12,
     marginBottom: 15,
+    borderColor: "#ccc",
+    backgroundColor: "#fff",
   },
-  dateLabel: {
+  label: {
     alignSelf: "flex-start",
-    marginBottom: 8,
+    marginBottom: 4,
+    marginTop: 4,
     fontSize: 16,
+    color: "#2E2E2E",
   },
   dateContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    marginBottom: 25,
+    marginBottom: 15,
     width: "100%",
   },
-  picker: {
-    flex: 1,
-    height: 50,
-    marginHorizontal: 5,
+  pickerDay: {
+    height: 55,
+    width: 93,
     borderColor: "#ccc",
+    borderRadius: 10,
     borderWidth: 1,
+    backgroundColor: "#fff",
+  },
+  pickerMonth: {
+    height: 55,
+    width: 120,
+    marginHorizontal: 3,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: "#fff",
+  },
+  pickerYear: {
+    height: 55,
+    width: 110,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: "#fff",
+  },
+  pickerInner: {
+    width: "100%",
+    height: 55,
   },
   signUpButton: {
-    backgroundColor: "#fff",
-    borderColor: "#000",
     borderWidth: 1,
     paddingVertical: 12,
     paddingHorizontal: 30,
     borderRadius: 8,
-    marginTop: 10,
+    alignSelf: "flex-end",
+    borderColor: "#ff9197",
+    backgroundColor: "#fcd7d7",
   },
   signUpText: {
     fontSize: 16,
     fontWeight: "600",
+    color: "#2E2E2E",
   },
   dividerContainer: {
     flexDirection: "row",
     alignItems: "center",
-    marginVertical: 30,
+    marginVertical: 25,
     width: "100%",
   },
   line: {
@@ -207,18 +236,22 @@ const styles = StyleSheet.create({
   },
   or: {
     marginHorizontal: 10,
-    fontSize: 16,
+    fontSize: 20,
     color: "#333",
   },
   loginButton: {
-    borderColor: "#000",
     borderWidth: 1,
     paddingVertical: 14,
-    paddingHorizontal: 90,
+    width: "100%",
     borderRadius: 10,
+    marginBottom: 50,
+    borderColor: "#ff9197",
+    backgroundColor: "#fcd7d7",
   },
   loginText: {
     fontSize: 16,
     fontWeight: "600",
+    alignSelf: "center",
+    color: "#2E2E2E",
   },
 });
